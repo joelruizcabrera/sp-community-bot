1da64a5c9221dbc8e29cb3719900fed0ef5b272e
joelruizcabrera <joel.schwegmann@gmx.de>
2021-04-20 12:11:34 +0200

    update

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,13 @@
-exports.run = (bot, message, [mention, ...reason]) => {
-    const modRole = message.guild.roles.cache.find(role => role.name === "Mod");
-    if (!modRole)
-        return console.log("The Mods role does not exist");
-
-    if (!message.member.roles.cache.has(modRole.id))
-        return message.reply("Du kannst diesen Befehl nicht ausführen");
-
-    if (message.mentions.members.size === 0)
-        return message.reply("Bitte erwähne einen Benutzer");
-
-    if (!message.guild.me.hasPermission("BAN_MEMBERS"))
-        return message.reply("");
-
-    const kickMember = message.mentions.members.first();
-
-    kickMember.ban(reason.join(" ")).then(member => {
-        message.reply(`${member.user.username} was succesfully banned.`);
-    });
+exports.run = (bot, msg, [mention, ...reason]) => {
+    if (msg.member.hasPermission("BAN_MEMBERS")) {
+        if (msg.members.mentions.first()) {
+            try {
+                msg.members.mentions.first().ban();
+            } catch {
+                msg.reply("I do not have permissions to ban" + msg.members.mentions.first());
+            }
+        } else {
+            msg.reply("You do not have permissions to ban" + msg.members.mentions.first());
+        }
+    }
 };
