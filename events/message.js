2584616dede681061b51aa79fdc9ddefa7e7febd
joelruizcabrera <joel.schwegmann@gmx.de>
2021-04-20 14:26:16 +0200

    update

--- events/message.js
+++ events/message.js
@@ -1,9 +1,12 @@
 module.exports = (bot, message) => {
     if (message.author.bot) return;
 
-    console.log("\x1b[47m\x1b[30m\x1b[1m[#" + message.channel.name + "]\x1b[0m \x1b[1m" + message.author.tag + ":\x1b[0m " + message.content);
 
-    if (message.content.indexOf(bot.config.prefix) !== 0) return;
+    if (message.content.indexOf(bot.config.prefix) !== 0) {
+        console.log("\x1b[47m\x1b[30m\x1b[1m[#" + message.channel.name + "]\x1b[0m \x1b[1m" + message.author.tag + ":\x1b[0m " + message.content);
+    } else {
+        console.log("\x1b[43m\x1b[30m[#" + message.channel.name + "] " + message.author.tag + ":\x1b[0m " + message.content);
+    }
 
     const args = message.content.slice(bot.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
