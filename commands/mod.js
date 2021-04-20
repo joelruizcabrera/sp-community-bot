b259390fd7e3688073cbf15121ab48d37a7ba12d
Joel Schwegmann <schwegmann@die-etagen.de>
2021-04-20 09:53:38 +0200

    update

--- commands/mod.js
+++ commands/mod.js
@@ -1,18 +1,24 @@
-exports.run = (bot, message, [mention, ...reason]) => {
+exports.run = (bot, message, args) => {
     const modRole = message.guild.roles.cache.find(role => role.name === "Mod");
-    if (!modRole)
+
+    message.reply(args);
+
+    if (!modRole) {
         return console.log("The Mods role does not exist");
+    }
 
-    if (!message.member.roles.cache.has(modRole.id))
+    if (!message.member.roles.cache.has(modRole.id)) {
         return message.reply("Du kannst diesen Befehl nicht ausführen");
+    }
 
-    if (message.mentions.members.size === 0)
+    if (message.mentions.members.size === 0) {
         return message.reply("Bitte erwähne einen Benutzer");
+    }
 
-    if (!message.guild.me.hasPermission("KICK_MEMBERS"))
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
         return message.reply("");
+    }
 
     const removeMod = message.mentions.members.first();
-
     removeMod.roles.remove(modRole);
 };
