63c62c007b6efce3016a2e0a85e0484417183144
Joel Schwegmann <schwegmann@die-etagen.de>
2021-04-20 10:22:22 +0200

    update

--- commands/mod.js
+++ commands/mod.js
@@ -33,6 +33,8 @@ exports.run = (bot, message, args) => {
         const removeMod = message.mentions.members.first();
         removeMod.roles.add(modRole);
 
+        console.log(removeMod);
+
         FUNCTIONS.conLink("INSERT INTO sp_mods (admin_dsc_id) VALUES (123435)");
     }
 };
